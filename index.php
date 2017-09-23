8b97cc23c22342a8ae50013a26e96f3070acff31
Snail <moskan9595@gmail.com>
2017-09-24 00:20:13 +0500

    A simple of bootstarp Page initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>Build e-commerce with php and bootstrap</title>
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top" >
+  <div class="container">
+    <a href="/bootstrap/index.php" class="navbar-brand">Red Stone shop</a>
+    <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Men <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="background-image">
+  <div id="image-1"></div>
+  <div id="image-2"></div>
+</div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
